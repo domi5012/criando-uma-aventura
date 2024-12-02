2a5a9169b0fbf1dd031a8d3df4c5a42f0ef55fd0
domi5012 <00001131997505sp@al.educacao.sp.gov.br>
2024-12-02 11:14:29 -0300

    envio final do projeto do algoritmo

---
+++ script.js
@@ -0,0 +1,11 @@
+const avanca = document.queryselectorAll('.btn-proximo');
+
+avanca.forEach(button=>{
+      button.addEventListener('click',function(){
+        const atual = document.queryselector('.ativo');
+        const proximopasso='passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximopasso),classList.add('ativo');
+      })
+    })
